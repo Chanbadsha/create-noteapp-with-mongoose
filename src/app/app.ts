ffca0c5d271230b5209a8c12a67a0802b82e0900
Chan Badsha Bhuiyan <chanbadsha005@gmail.com>
2025-08-05 12:35:59 +0600

    create schema and add 1st note

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,31 @@
 import express, { Application, Request, Response } from 'express'
+import { model, Schema } from 'mongoose'
 const app: Application = express()
 
 
+
+
+// Create schema for all notes
+const noteSchema = new Schema({
+    title: String,
+    content: String
+})
+
+const Note = model("Note", noteSchema)
+
+
+app.post('/create-note', async (req: Request, res: Response) => {
+    const myNote = new Note({
+        title: 'I am learning Mongoose',
+        content: "This is learning process of mongoose schema"
+    })
+    await myNote.save()
+    res.status(201).json({
+        success: true,
+        message: "A note created successfully",
+        note: myNote
+    })
+})
 app.get('/', (req: Request, res: Response) => {
     res.send("Welcome To NoteApp Server")
 })
--- src/app/server.ts
+++ src/app/server.ts
@@ -8,7 +8,7 @@ let server: Server
 async function main() {
 
     try {
-        await mongoose.connect('mongodb+srv://nextLevelUser:nextLevelUser@cluster0.t47d6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+        await mongoose.connect('mongodb+srv://nextLevelUser:nextLevelUser@cluster0.t47d6.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0')
         server = app.listen(port, () => {
             console.log(`The server is running on port ${port}`)
         })
